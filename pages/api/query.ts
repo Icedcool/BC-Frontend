4b3d2abbb37820f0ab0ce682b0329df0083beae4
John <50239591+Icedcool@users.noreply.github.com>
2022-12-20 22:49:32 -0500

    Query
    Updated index to include the query, and updated query to return dummy data.

---
+++ pages/api/query.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  transactions: any[]
+}
+
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'bc1.c6g7dlo9dbal.us-east-1.rds.amazonaws.com',
+//   user     : 'me',
+//   password : 'secret',
+//   database : 'my_db'
+// });
+
+// connection.connect();
+
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+
+// connection.end();
+
+const DUMMY = { transactions: [{id:'John Doe'},{id:'John Doe'},{id:'John Doe'}] };
+
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json(DUMMY)
+}
