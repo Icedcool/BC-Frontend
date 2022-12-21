f70b5f63bde47bbfbf8fcbd64bd5b5f1a36fe662
John <50239591+Icedcool@users.noreply.github.com>
2022-12-21 11:21:22 -0500

    Update query.ts
    Updated new connection info.

--- pages/api/query.ts
+++ pages/api/query.ts
@@ -6,22 +6,22 @@ type Data = {
 }
 console.log(process.env.MYSQLPASSWORD)
 
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'bc1.c6g7dlo9dbal.us-east-1.rds.amazonaws.com',
-//   user     : 'me',
-//   password : 'secret',
-//   database : 'my_db'
-// });
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'bc1.c6g7dlo9dbal.us-east-1.rds.amazonaws.com',
+  user     : 'tableau',
+  password : MYSQLPASSWORD,
+  database : 'finance'
+});
 
-// connection.connect();
+connection.connect();
 
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
+connection.query('Select cs.id as sowID, c.ClientName, cs.AmtUSDC, cs.Currency, cs.Sponsor, cs.BudgetURL, cs.SOWURL, cs.StartDate, cs.EndDate, cs.Completed, cs.ClientID, cs.ProjectTeamPercent from Clients c left join ClientSOW cs on c.id = cs.ClientID ', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
 
-// connection.end();
+connection.end();
 
 const DUMMY = { transactions: [{id:'John Doe'},{id:'John Doe'},{id:'John Doe'}] };
 
