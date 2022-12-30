20e48ff8069f71081088646f69b4a1065ab1ecd6
John <50239591+Icedcool@users.noreply.github.com>
2022-12-29 22:19:25 -0500

    Update index.tsx
    Removing fake.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import { Inter } from '@next/font/google'
 //import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react'
-import fake from "../data/fake.json"
+// import fake from "../data/fake.json"
 
 interface RowDataPacket{
   sowID: number,
